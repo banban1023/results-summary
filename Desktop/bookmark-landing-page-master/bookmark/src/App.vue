031a2a89ecdc57f3b458fc7d1c545c6f1bc2e838
板板 <747657490@qq.com>
2025-06-09 18:02:41 +0800

    start

---
+++ Desktop/bookmark-landing-page-master/bookmark/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bookmark">
+    <header>
+      <h1 class="bookmark_logo">bookmark</h1>
+      <nav class="bookmark_nav">
+        <button class="bookmark_nav_btn"></button>
+      </nav>
+      <img src="./assets/images/illustration-hero.svg" alt="" class="bookmark_header_img">
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import './assets/styles/index.less';
+</style>
